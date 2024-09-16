3417cfb0efde9cceaa1f0cf85ca035ac499afe65
WockhardtJah <jahlacaims@gmail.com>
2024-09-16 13:28:06 -0400

    Update string.cs

--- 00_computer_science_exercises/03_strings/string.cs
+++ 00_computer_science_exercises/03_strings/string.cs
@@ -12,9 +12,36 @@ class FlowControlStructres {
     // // Can I vote?
     // if (age >=18) // (age >=18) is the CONDITIONAL STATEMENT
     // {
-    //    Console.Writeline    
-    }
-}
+    //      Console.Writeline("You can vote!\n");
+    // }    
+    // elese if (age >= 16) // Check this NEXT if Line #13 is false.
+    // {
+    //     Console.WriteLine("You can't vote but you CAN register.\n");
+    // }
+    // else 
+    // {
+    //    Console.WriteLine("You are not old enough to vote or register. Scrub.\n")
+    // }
+
+    // /*
+    // You can have a single if statement by itslef.
+    // If you have an else you MUST also have an if.
+    // You can havve as many else if statemnts as needed.
+    // FOR NOW, USE THE else CONDITION TO PRINT ERROR MESSAGES / LOGS
+    // */
+
+    // int age; 
+    // if (age >= 0)
+    // {
+    //   Console.WriteLine("Age data stored.\n");
+    // }
+    // else 
+    // {
+    //     Console.WriteLine("No age value detected. Please review. \n");
+    // }
+
+    // // Switch Statements
+    // int day = 1;
 
     
 
