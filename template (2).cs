0d4ef3f700d26c3a4068a25cb397aa41cd51134e
WockhardtJah <jahlacaims@gmail.com>
2024-10-30 13:09:23 -0400

    Create template (2).cs

---
+++ template (2).cs
@@ -0,0 +1,27 @@
+// Collection example, Jasher Douglas, v0.0
+using System;
+using System.Linq;
+using System.Collections;
+
+class Collections {
+ class Template {
+  static void Main() {
+    
+  }
+
+
+
+
+
+-- POSITION OF THE ELEMENT IS THE INDEX
+-- FIRST ELEMENT INDEX IS 0.
+*/
+
+// DECLARING AND DEFINING AN ARRAY
+string[] breakfastFoods = {"Sausage","Eggs","Chicken","Cereal","Fried Snake nuts"};
+int[] testScore = {0, 25, 15, 75, 99}
+double[] GPA = {0.25D, 2.99D, 5.05D, 0.0D, 1.11D};
+
+// Print Array Contents -- ALl Elements on Single Line 
+Console.WriteLine("The Elements ofn each array are:\n");
+Console.WriteLine("breakfastFoods:\n
